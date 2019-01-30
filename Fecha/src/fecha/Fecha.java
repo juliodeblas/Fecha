8728a5d977009c583b9e43063bff2b13d718de63
Julio de Blas <julio.deblas@cesjuanpablosegundo.es>
2019-01-30 12:16:36 +0100

    Fecha

--- Fecha/src/fecha/Fecha.java
+++ Fecha/src/fecha/Fecha.java
@@ -28,19 +28,19 @@ public class Fecha {
         System.out.print("Introduce la hora: ");
         hora = sc.nextLine();
 
-        if (dia.equals("lunes") && 1 < Integer.parseInt(hora.substring(0, 2)) && Integer.parseInt(hora.substring(0, 2)) < 24 && 0 < Integer.parseInt(hora.substring(3, 5)) && Integer.parseInt(hora.substring(3, 5)) < 60) {
+        if (dia.equals("lunes") && 00 <= Integer.parseInt(hora.substring(0, 2)) && Integer.parseInt(hora.substring(0, 2)) < 24 && 0 <= Integer.parseInt(hora.substring(3, 5)) && Integer.parseInt(hora.substring(3, 5)) < 60) {
             minutos = 24 * 60 - (Integer.parseInt(hora.substring(0, 2)) * 60 + Integer.parseInt(hora.substring(3, 5))) + 5220;
             System.out.println("Faltan " + minutos + " minutos para el fin de semana.");
-        } else if (dia.equals("martes") && 1 < Integer.parseInt(hora.substring(0, 2)) && Integer.parseInt(hora.substring(0, 2)) < 24 && 0 < Integer.parseInt(hora.substring(3, 5)) && Integer.parseInt(hora.substring(3, 5)) < 60) {
+        } else if (dia.equals("martes") && 00 <= Integer.parseInt(hora.substring(0, 2)) && Integer.parseInt(hora.substring(0, 2)) < 24 && 0 <= Integer.parseInt(hora.substring(3, 5)) && Integer.parseInt(hora.substring(3, 5)) < 60) {
             minutos = 24 * 60 - (Integer.parseInt(hora.substring(0, 2)) * 60 + Integer.parseInt(hora.substring(3, 5))) + 3780;
             System.out.println("Faltan " + minutos + " minutos para el fin de semana.");
-        } else if (dia.equals("miercoles") && 1 < Integer.parseInt(hora.substring(0, 2)) && Integer.parseInt(hora.substring(0, 2)) < 24 && 0 < Integer.parseInt(hora.substring(3, 5)) && Integer.parseInt(hora.substring(3, 5)) < 60) {
+        } else if (dia.equals("miercoles") && 00 <= Integer.parseInt(hora.substring(0, 2)) && Integer.parseInt(hora.substring(0, 2)) < 24 && 0 <= Integer.parseInt(hora.substring(3, 5)) && Integer.parseInt(hora.substring(3, 5)) < 60) {
             minutos = 24 * 60 - (Integer.parseInt(hora.substring(0, 2)) * 60 + Integer.parseInt(hora.substring(3, 5))) + 2340;
             System.out.println("Faltan " + minutos + " minutos para el fin de semana.");
-        } else if (dia.equals("jueves") && 1 < Integer.parseInt(hora.substring(0, 2)) && Integer.parseInt(hora.substring(0, 2)) < 24 && 0 < Integer.parseInt(hora.substring(3, 5)) && Integer.parseInt(hora.substring(3, 5)) < 60) {
+        } else if (dia.equals("jueves") && 00 <= Integer.parseInt(hora.substring(0, 2)) && Integer.parseInt(hora.substring(0, 2)) < 24 && 0 <= Integer.parseInt(hora.substring(3, 5)) && Integer.parseInt(hora.substring(3, 5)) < 60) {
             minutos = 24 * 60 - (Integer.parseInt(hora.substring(0, 2)) * 60 + Integer.parseInt(hora.substring(3, 5))) + 900;
             System.out.println("Faltan " + minutos + " minutos para el fin de semana.");
-        } else if (dia.equals("viernes") && 1 < Integer.parseInt(hora.substring(0, 2)) && Integer.parseInt(hora.substring(0, 2)) < 14 && 0 < Integer.parseInt(hora.substring(3, 5)) && Integer.parseInt(hora.substring(3, 5)) < 60) {
+        } else if (dia.equals("viernes") && 00 <= Integer.parseInt(hora.substring(0, 2)) && Integer.parseInt(hora.substring(0, 2)) < 14 && 0 <= Integer.parseInt(hora.substring(3, 5)) && Integer.parseInt(hora.substring(3, 5)) < 60) {
             minutos = 15 * 60 - (Integer.parseInt(hora.substring(0, 2)) * 60 + Integer.parseInt(hora.substring(3, 5)));
             System.out.println("Faltan " + minutos + " minutos para el fin de semana.");
         } else {
